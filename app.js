d36d5cee4f9418d5ebe01d54bdc50d784cac5bd3
Pim Dubois <pim.dubois@capgemini.com>
2020-03-06 12:47:13 +0100

    basic functionality for fullscreen popover

---
+++ app.js
@@ -0,0 +1,29 @@
+var data = {
+  0: {
+    title: "title0",
+    description: "lorem 0..."
+  },
+  1: {
+    title: "title1",
+    description: "lorem 1..."
+  },
+  2: {
+    title: "title2",
+    description: "lorem 2..."
+  }
+};
+
+$("button").on("click tap", function() {
+  var $this = $(this),
+    $popover = $(".popover"),
+    $popoverTarget = $this.data("target");
+
+  $popover.removeClass("closed");
+  $popover.attr("data-target-id", $popoverTarget);
+  $popover.find(".body h2").text(data[$popoverTarget].title);
+  $popover.find(".body p").text(data[$popoverTarget].description);
+});
+
+$(".close").on("click tap", function() {
+  $(".popover").addClass("closed");
+});
